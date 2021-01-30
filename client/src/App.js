08ae083bc83a94a9082fc98a90fbdf9e6b25d306
waysketch <waysketch@gmail.com>
2021-01-30 16:28:55 -0700

    created router for root level of app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,21 @@
+import React from "react";
+import {Route, Switch} from "react-router-dom";
+import Home from './pages/Home/Home.jsx';
+import Login from './pages/Login/Login.jsx';
+import Error from './pages/Error/Error.jsx';
+
 function App() {
   return (
-    <h1>hello world</h1>
+    <Switch>
+      {/* HOME PAGE */}
+      <Route exact path="/" render={() => <Home />} />
+
+      {/* LOGIN PAGE */}
+      <Route exact path="/login" render={() => <Login />} />
+
+      {/* DO NOT CODE BELOW THIS LINE */}
+      <Route render={() => <Error />} />
+    </Switch>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import allReducers from './redux/reducers';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+// theme
+
+const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    
+  </Provider>,
   document.getElementById('root')
 );
 
