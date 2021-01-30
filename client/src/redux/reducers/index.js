77409690c27a3e44846259eb6f99b67469f0000d
waysketch <waysketch@gmail.com>
2021-01-30 16:40:24 -0700

    updated routes and ended stream

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,9 +1,10 @@
 // === STATE === //
+import {combineReducers} from 'redux';
 
 import isLoggedIn_reducer from './isLoggedIn.js';
 
 const allReducers = combineReducers({
-    isLoggedIn: isLoggedIn_reducer,
+    isLoggedIn: isLoggedIn_reducer
 });
 
 export default allReducers;
